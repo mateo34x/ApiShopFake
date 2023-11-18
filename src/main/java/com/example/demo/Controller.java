25b550779d70d5d40c9c108b098e265780257926
Mateo Rodriguez <mateo15rg@gmail.com>
2023-11-17 23:56:12 -0500

    Conexión establecida con DB

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/findAll")
+public class Controller {
+
+    @Autowired
+    ItemRepository groceryItemRepo;
+
+
+   /* @GetMapping
+    public void createGroceryItems() {
+        System.out.println("Data creation started...");
+        groceryItemRepo.save(new GroceryItem("Whole Wheat Biscuit", "Whole Wheat Biscuit", 5, "snacks"));
+        groceryItemRepo.save(new GroceryItem("Kodo Millet", "XYZ Kodo Millet healthy", 2, "millets"));
+        groceryItemRepo.save(new GroceryItem("Dried Red Chilli", "Dried Whole Red Chilli", 2, "spices"));
+        groceryItemRepo.save(new GroceryItem("Pearl Millet", "Healthy Pearl Millet", 1, "millets"));
+        groceryItemRepo.save(new GroceryItem("Cheese Crackers", "Bonny Cheese Crackers Plain", 6, "snacks"));
+        System.out.println("Data creation complete...");
+    }*/
+
+
+    @GetMapping(produces = "application/json")
+    @ResponseBody
+    public List<GroceryItem> getAllData() {
+
+        return groceryItemRepo.findAll();
+    }
+}
