8b536261629991e8d5af17a8d62c089504730abc
Mateo Rodriguez <mateo15rg@gmail.com>
2023-11-26 17:20:56 -0500

    new function test "UploadImage" to Cloudinary

--- src/main/java/com/example/demo/CloudinaryService.java
+++ src/main/java/com/example/demo/CloudinaryService.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
 
+@Service
 public class CloudinaryService {
 
     @Value("${cloudinary.cloud-name}")
